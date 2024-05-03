aae4bf24ab6e4fc49cbba66a364e2ee202a0d738
tavozalixi <143713772+tavozalixi@users.noreply.github.com>
2024-05-03 12:26:59 -0300

    Add files via upload

--- Projeto-de-vida-main/main.js
+++ Projeto-de-vida-main/main.js
@@ -1,8 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
+
 
 for(let i=0;1 <botoes.lenght;i++){
-    botoes[i].onclick = function()
-    console.log(botoes[i]);
+    botoes[i].onclick = function(){
+        botoes[i].classList.add("ativo");
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classlist.remove("ativo");
+        }
+
+    }
+    
 
 }
